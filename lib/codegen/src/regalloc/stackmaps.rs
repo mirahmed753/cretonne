5e2f7a7ebeb46a9e4aaa8bf2b276c1d5fdda84b9
Mir Ahmed <mirahmed753@gmail.com>
2018-07-30 15:46:00 -0700

    changed stackmaps.rs

--- lib/codegen/src/regalloc/stackmaps.rs
+++ lib/codegen/src/regalloc/stackmaps.rs
@@ -1,30 +1,23 @@
-use ir::Function; //Inst
+use ir::Function;
 use isa::TargetIsa;
 use dominator_tree::DominatorTree;
 use regalloc::live_value_tracker::LiveValueTracker;
 use regalloc::liveness::Liveness;
 use cursor::{Cursor, FuncCursor};
 use std::collections::HashSet;
-// use ir::instructions::{BranchInfo, CallInfo, InstructionData};
-// use ir::builder::InstBuilder;
-use ir::{self, InstBuilder};
+use ir::InstBuilder;
 
-// use ir::entities::Inst
-// use regalloc::RegDiversions;
-
-// emit_stackmaps() function prints the Instruction and the liveness of its defs and ops
-// by traversing the dominator tree in a post order fashion
+// The emit_stackmaps() function analyzes each instruction to retrieve the liveness of
+// the defs and operands by traversing the dominator tree in a post order fashion.
 pub fn emit_stackmaps(isa: &TargetIsa, func: &mut Function, domtree: &mut DominatorTree,
     liveness: &mut Liveness, tracker: &mut LiveValueTracker) {
 
     // Visit EBBs in post-order
-    // A `FuncCursor` holds a mutable reference to a whole `ir::Function` while keeping a position
-    // too. The function can be re-borrowed by accessing the public `cur.func` member.
     let mut pos = FuncCursor::new(func);
     let mut ebbs_for_stackmap = HashSet::new();
 
     for &ebb in domtree.cfg_postorder().iter() {
-        println!("Ebb Number is {:?}", ebb);
+
         // call ebb_top && drop_dead_params
         tracker.ebb_top(ebb, &pos.func.dfg, liveness, &pos.func.layout, domtree);
         tracker.drop_dead_params();
@@ -43,8 +36,6 @@ pub fn emit_stackmaps(isa: &TargetIsa, func: &mut Function, domtree: &mut Domina
                 // Find what the branch destination is
                 let branch_dest = pos.func.dfg[inst].branch_destination();
                 ebbs_for_stackmap.insert(branch_dest);
-
-                println!("{}", opcode);
             }
 
             // Process the instruction
@@ -66,7 +57,9 @@ pub fn emit_stackmaps(isa: &TargetIsa, func: &mut Function, domtree: &mut Domina
                 }
             }
 
-            // live_value_list will have the list of live values in this instruction
+            // live_value_list will have the list of live values for the instruction
+            // that we are currently working with
+
             // print contents of array
             println!("In {:?}, {:?} has live values: ", ebb, inst);
             print!("   ");
@@ -84,6 +77,7 @@ pub fn emit_stackmaps(isa: &TargetIsa, func: &mut Function, domtree: &mut Domina
         } // end while loop for instructions
     } // end for loop for ebb
 
+    // Note: Using a HashSet to insert the stackmap instructions is a temporary method
     // loop through items in HashSet to insert stackmap instruction
     for ebb in ebbs_for_stackmap {
         pos.goto_first_insertion_point(ebb.unwrap());
@@ -92,7 +86,7 @@ pub fn emit_stackmaps(isa: &TargetIsa, func: &mut Function, domtree: &mut Domina
         let mut value_list = Vec::new();
         pos.ins().stackmap(&value_list);
 
-        // println!("Inserted Instruction Data: {}", pos.func.dfg.display_inst(pos.current_inst().unwrap(), None));
+        println!("Inserted Instruction Data: {}", pos.func.dfg.display_inst(pos.current_inst().unwrap(), None));
 
     }
 }
